41d656c4218d2f3aa39d18bccef0d07db36f5efa
vaishnavasokan <vaishnavasokan050>
2019-08-01 01:06:59 +0530

    book

--- mydata2.js
+++ mydata2.js
@@ -2,65 +2,45 @@ $(document).ready(function()
 {
     $("#image").hide();
 
-    $("#btn1").click(function()
+    $("#category").change(function()
     {
         $("#image").show();
             $.ajax({
-                    type:"GET",
-                    url:"./books.json",
-                    success:function(p)
-                    {
-                        $("#image").hide();
-                        
-                        var output="<table class='table'><thead><tr><th>NAME</th><th>IMAGE</th><th>AUTHOR</th><th>CATEGORY</th><th>PRICE</th></th></thead>"
-                        //var output="";
-                        for(var i in p)
+                type:"GET",
+                url:"./books.json",
+                beforeSend: function(){
+                $("#image").show();
+                },
+                    
+                success:function(p)
+                {
+                    $("#image").hide();
+                    var ct=$("#category").val();
+                    var output="<table class='table'><thead><tr><th>NAME</th><th>IMAGE</th><th>AUTHOR</th><th>CATEGORY</th><th>PRICE</th></th></thead>"
+                    for(var i in p)
+                    {   
+                            /*output+="<tr><td>"+p[i].name+"</td>";
+                            output+="<td><img src='"+p[i].image1+"'width=150 height=150></td>";
+                            output+="<td>"+p[i].author+"</td>";
+                            output+="<td>"+p[i].category+"</td>";
+                            output+="<td>"+p[i].price+"</td></tr>";*/
+
+                        var c=p[i].category;
+                        if(c==ct)
                         {   
                             output+="<tr><td>"+p[i].name+"</td>";
                             output+="<td><img src='"+p[i].image1+"'width=150 height=150></td>";
                             output+="<td>"+p[i].author+"</td>";
                             output+="<td>"+p[i].category+"</td>";
                             output+="<td>"+p[i].price+"</td></tr>";
+
+                        }
                             
-                        }
-                        output+="</table>";
-                        $(".result").html(output);
                     }
-                });           
-    });
-
-    $("#category").mouseenter(function()
-    {
-
-        $("#ct").slideDown("slow");
-            $("#c1").click(function()
-            {
-                var cname=$("#c1").val();
-                
-                $("#ct").hide(250);
-
-                $.ajax({
-                        type:"GET",
-                        url:"./books.json",
-                        success:function(p)
-                        {
-                            console.log(cname);
-                            for(var i in p)
-                            {
-                                if(p[i].category==cname)
-                                {
-                                    output+="<tr><td>"+p[i].name+"</td>";
-                                    output+="<td><img src='"+p[i].image1+"'width=150 height=150></td>";
-                                    output+="<td>"+p[i].author+"</td>";
-                                    output+="<td>"+p[i].category+"</td>";
-                                    output+="<td>"+p[i].price+"</td></tr>";
-                                }
-                            }
-                            output+="</table>";
-                            $(".result").html(output);
-                        }
-                });
+                    output+="</table>";
+                    $(".result").html(output);
+                }
             });
-    });
+    });        
 
 });
